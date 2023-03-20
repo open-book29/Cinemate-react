61384b985e13907372206bb62098aab426ae7ee8
open-book29 <anujmaurya704@gmail.com>
2023-03-20 20:07:49 +0530

    udate footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,16 +10,16 @@ export default function Footer() {
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">About</a>
+              <Link to="/" rel='noreferrer' className="mr-4 hover:underline md:mr-6 ">About</Link>
             </li>
             <li>
-              <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+              <Link to="/" rel='noreferrer' className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
             </li>
             <li>
-              <a href="#" target="_blank" className="mr-4 hover:underline md:mr-6">Licensing</a>
+              <Link to="/"rel='noreferrer' className="mr-4 hover:underline md:mr-6">Licensing</Link>
             </li>
             <li>
-              <a href="#" target="_blank" className="hover:underline">Contact</a>
+              <Link to="/" rel='noreferrer' className="hover:underline">Contact</Link>
             </li>
           </ul>
         </div>
